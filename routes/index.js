b92ce5c0c5368d7c38a645a258446a087f5c067a
maoqiao <leon_zmq520@hotmail.com>
2014-11-14 02:30:12 +0100

    fix the GET/POST method router functions

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { });
+router.get('/index', function(req, res) {
+  res.render('index', { title: 'Home Page'});
 });
 
-/* GET login page. */
+/* GET/POST login page. */
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Login' });
+  if(req.session.isVisit != 0){
+    console.log('you have been ' + req.session.isVisit + ' time to come to this page');
+    req.session.isVisit++;
+    res.render('index',{ title: 'Profile Page'});
+  }else{
+    req.session.isVisit = 1;
+    console.log('you have been ' + req.session.isVisit + ' time to come to this page');
+    res.render('login', { title: 'Login' });
+  }
 });
 
-/* GET contact page. */
-router.get('/contact', function(req, res) {
-  res.render('contact', { title: 'Contact' });
+router.post('/login',function(req, res){
+  //out print
+  console.log('username :'+req.body.username);
+  console.log('psw :'+req.body.password);
+
+  if(req.body.username && req.body.password){
+    req.session.isVisit++;
+    res.render('index',{ title: 'Profile Page'});
+  }
+});
+
+/* GET logout page. */
+router.get('/logout', function(req, res) {
+  req.session.isVisit = 0;
+  res.render('index', { title: 'come back the Home Page!' });
 });
 
 module.exports = router;
